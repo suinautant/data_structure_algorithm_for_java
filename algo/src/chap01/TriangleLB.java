32cf51ae948cf83a079c9f396de0b38a6505e44d
suinautant <suinautant@gmail.com>
2021-07-30 18:33:03 +0900

    ch01 end

--- algo/src/chap01/TriangleLB.java
+++ algo/src/chap01/TriangleLB.java
@@ -18,16 +18,42 @@ public class TriangleLB {
 //		triangleLB(n);
 //		triangleLU(n);
 //		triangleRU(n);
-		triangleRB(n);
+//		triangleRB(n);
+//		spira(n);
+		npira(n);
+	}
+
+	static private void npira(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (i - 1) * 2 + 1; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+	static private void spira(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (i - 1) * 2 + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 	// 오른쪽 아래가 직각인 이등변 삼각형 출력
 	static void triangleRB(int n) {
 		for (int i = 1; i <= n; i++) {
-			for (int j = n; j >= 1; j--) {
+			for (int j = n - i; j >= 1; j--) {
 				System.out.print(" ");
 			}
-			for (int j = 1; j <= n; j++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
