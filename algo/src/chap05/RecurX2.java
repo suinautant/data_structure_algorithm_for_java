0f1ca3b5fb4d7e65a4f234fa730c92ace87435da
suinautant <suinautant@gmail.com>
2021-08-22 16:34:12 +0900

    recur and without recur

---
+++ algo/src/chap05/RecurX2.java
@@ -0,0 +1,31 @@
+package chap05;
+
+import java.util.Scanner;
+
+public class RecurX2 {
+	static void recur(int n) {
+		IntStack s = new IntStack(n);
+
+		while (true) {
+			if (n > 0) {
+				s.push(n);
+				n--;
+				continue;
+			}
+			if (s.isEmpty() != true) {
+				n = s.pop();
+				System.out.println(n);
+				n -= 2;
+				continue;
+			}
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int x = sc.nextInt();
+		recur(x);
+	}
+}
